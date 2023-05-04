ae507b4e514db49712564bafeb6200b252d76173
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-05 00:36:39 +0600

    chefs data added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 7000;
+var cors = require("cors");
+const port = process.env.PORT || 7000;
+
+const chefs = require("./data/chefs.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("food-cravings!");
 });
 
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
 app.listen(port, () => {
-  console.log(`food-cravings listening on port ${port}`);
+  console.log(`food-cravings listening on port: ${port}`);
 });
